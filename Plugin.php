octoshop/categories

Add plugin skeleton

Plugin.php
@@ -1,0 +1,20 @@
+<?php namespace Octoshop\TreeCat;
+
+use Backend;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = ['Octoshop.Core'];
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'octoshop.treecat::lang.plugin.name',
+            'icon' => 'icon-shopping-cart',
+            'author' => 'Dave Shoreman',
+            'homepage' => 'http://octoshop.co/',
+            'description' => 'octoshop.treecat::lang.plugin.description',
+        ];
+    }
+}

octoshop/checkout

Plugin.php
@@ -1,0 +1,20 @@
+<?php namespace Octoshop\Checkout;
+
+use Backend;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public $require = ['Octoshop.Core'];
+
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'octoshop.checkout::lang.plugin.name',
+            'icon' => 'icon-shopping-cart',
+            'author' => 'Dave Shoreman',
+            'homepage' => 'http://octoshop.co/',
+            'description' => 'octoshop.checkout::lang.plugin.description',
+        ];
+    }
+}